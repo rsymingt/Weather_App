0a35b6f3489ce772e37ca00e944f0f6ae1197061
rsymingt <rsymingt@mail.uoguelph.ca>
2016-12-22 02:19:06 -0500

    Finished!

--- app/src/main/java/com/example/rsymi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rsymi/myapplication/MainActivity.java
@@ -1,15 +1,62 @@
 package com.example.rsymi.myapplication;
 
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Icon;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+
+import data.CityPreferences;
+import data.JSONWeatherParser;
+import data.WeatherHttpClient;
+import model.Weather;
+import util.Utils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView cityName;
+    private TextView temp;
+    private ImageView iconView;
+    private TextView description;
+    private TextView humidity;
+    private TextView pressure;
+    private TextView wind;
+    private TextView sunrise;
+    private TextView sunset;
+    private TextView updated;
+    private Button btnUpdate;
+    private TextView updatingText;
+
+    private Weather weather = new Weather();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +65,181 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        cityName = (TextView) findViewById(R.id.cityText);
+        iconView = (ImageView) findViewById(R.id.thumbnailIcon);
+        temp = (TextView) findViewById(R.id.tempText);
+        description = (TextView) findViewById(R.id.cloudText);
+        humidity = (TextView) findViewById(R.id.humidText);
+        pressure = (TextView) findViewById(R.id.pressureText);
+        wind = (TextView) findViewById(R.id.windText);
+        sunrise = (TextView) findViewById(R.id.riseText);
+        sunset = (TextView) findViewById(R.id.setText);
+        updated = (TextView) findViewById(R.id.updateText);
+        btnUpdate = (Button) findViewById(R.id.update);
 
+        updatingText = (TextView) findViewById(R.id.updatingText);
+
+        final Animation fadeUpdate = new AlphaAnimation(1f, 0f);
+
+        fadeUpdate.setDuration(1000);
+
+        fadeUpdate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                updatingText.setText("Updating...");
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                updatingText.setText("");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CityPreferences cityPreferences = new CityPreferences(MainActivity.this);
+
+                renderWeatherData(cityPreferences.getCity());
+
+
+                updatingText.startAnimation(fadeUpdate);
+            }
+        });
+
+        CityPreferences cityPreferences = new CityPreferences(MainActivity.this);
+
+        renderWeatherData(cityPreferences.getCity());
+
+    }
+
+    public void renderWeatherData(String city){
+        WeatherTask weatherTask = new WeatherTask();
+
+        try {
+            weather = weatherTask.execute(new String[]{city + "&units=metric&appid=74121be7bd89c3b14470a7cf02a337d9"}).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        weather.iconData = weather.currentCondition.getIcon();
+
+        new DownloadImageAsyncTask().execute(weather.iconData);
+    }
+
+    private class DownloadImageAsyncTask extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            return downloadImage(params[0]);
+        }
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            iconView.setImageBitmap(bitmap);
+            super.onPostExecute(bitmap);
+        }
+
+        private Bitmap downloadImage(String code) {
+            try {
+                final HttpURLConnection client = (HttpURLConnection) new URL(Utils.ICON_URL + code + ".png").openConnection();
+                client.connect();
+
+                final int statusCode = client.getResponseCode();
+
+                if (statusCode == -1) {
+                    Log.e("DownloadImage", "Error:" + statusCode);
+                    return null;
+                }
+
+                Object obj = client.getContent();
+                if (obj != null) {
+                    InputStream inputStream = null;
+
+                    inputStream = client.getInputStream();
+
+                    //decode contents
+                    final Bitmap bitMap = BitmapFactory.decodeStream(inputStream);
+                    return bitMap;
+                }
+
+                client.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    private class WeatherTask extends AsyncTask<String, Void, Weather> {
+        @Override
+        protected Weather doInBackground(String... params) {
+
+            String data = ((new WeatherHttpClient()).getWeatherData(params[0]));
+
+            weather = JSONWeatherParser.getWeather(data);
+
+            return weather;
+        }
+
+        @Override
+        protected void onPostExecute(Weather weather) {
+            super.onPostExecute(weather);
+
+            DateFormat df = DateFormat.getTimeInstance();
+
+            String sunriseDate = df.format(new Date(weather.place.getSunrise()));
+            String sunsetDate = df.format(new Date(weather.place.getSunset()));
+            String lastUpdateDate = df.format(new Date(weather.place.getLastUpdate()));
+
+            DecimalFormat decimalFormat = new DecimalFormat("#.#");
+
+            String tempFormat = decimalFormat.format(weather.currentCondition.getTemperature());
+
+            cityName.setText(weather.place.getCity() + ", " + weather.place.getCountry());
+            temp.setText("" + tempFormat + "C");
+            humidity.setText("Humidity: " + weather.currentCondition.getHumidity() + "%");
+            pressure.setText("Pressure: " + weather.currentCondition.getPressure() + "hPa");
+            wind.setText("Winds: " + weather.wind.getSpeed() + "mps");
+            sunrise.setText("Sunrise: " + sunriseDate);
+            sunset.setText("Sunset: " + sunsetDate);
+            description.setText("Condition: " + weather.currentCondition.getCondition() + " (" +
+            weather.currentCondition.getDescription() + ")");
+
+            updated.setText("Last Updated: " + lastUpdateDate);
+        }
+    }
+
+    private void showInputDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Change City");
+
+        final EditText cityInput = new EditText(MainActivity.this);
+        cityInput.setInputType(InputType.TYPE_CLASS_TEXT);
+        cityInput.setHint("Guelph,can");
+        builder.setView(cityInput);
+        builder.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(cityInput.getText().toString().length() > 0) {
+                    CityPreferences cityPreferences = new CityPreferences(MainActivity.this);
+                    cityPreferences.setCity(cityInput.getText().toString());
+
+                    String newCity = cityPreferences.getCity();
+
+                    Log.e("MAIN:", newCity);
+
+                    renderWeatherData(newCity);
+                }
+            }
+        });
+        builder.show();
     }
 
     @Override
@@ -37,7 +258,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.change_cityId) {
-            return true;
+            showInputDialog();
         }
 
         return super.onOptionsItemSelected(item);
